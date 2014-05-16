16a792a0ab4f742d066353a07523a831540aff07
arnaudbouffard <arn0n0@hotmail.com>
2014-05-16 16:56:37 +0200

    added ingredients controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Cockteles::Application.routes.draw do
   root 'welcome#index'
 
   resources :cocktails
+  resources :ingredients
     # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
