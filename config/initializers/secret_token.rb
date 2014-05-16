e6eeacc4283e372694251811c9fb10ba048ee6e0
arnaudbouffard <arn0n0@hotmail.com>
2014-05-16 11:38:58 +0200

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cockteles::Application.config.secret_key_base = '25306f3cee15c034779569b4a7128c20d7d11f1faf71c0a4e1dba374dbfec14c5af9597fced02f4a6fec3fedb0ebf1527ebbbd34106d4c143db726884d694799'
